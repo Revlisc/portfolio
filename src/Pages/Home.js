7d5a8a5e499bd4936582b03fd2ce70e31842170b
dales <dalesimmons0210@gmail.com>
2021-09-09 21:55:23 -0700

    fonts

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,17 +9,7 @@ const Home = () => {
             <Row>
                 <h1 className='openingline'>Hi, I'm Dale</h1>
             </Row>
-            <Row>
-                <h3 className='ima'>
-                    I'm a {' '}
-                    {/* <Typical
-                        steps={['Developer', 3500, 'Scientist', 3500, 'Team Member', 3500, 'Problem Solver', 3500]}
-                        loop={Infinity}
-                        className='typewriter'
-                    /> */}
-                    
-                </h3>
-            </Row>
+            
             <Row className='iconslinks'>
                 <a href='https://github.com/Revlisc' alt='github address Revlisc' ><i className={'fa-github fab hicon'} /></a>
                 <a href='https://www.linkedin.com/in/dalejsimmons/' alt='my linkedin profile' ><i className="fab fa-linkedin-in hicon" /></a>
