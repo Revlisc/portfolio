a4fa6143e3afa524e2873118ecb74add15ae8b88
dales <dalesimmons0210@gmail.com>
2023-03-06 22:18:27 -0800

    updated timeline page

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -12,43 +12,36 @@ const Timeline = () => {
                 <Row>
                     <h2 className='resumeSectionHeader'>Experience</h2>
                 </Row>
+
+                <Row className='timelineRow'>
+                    <Entry 
+                        title={'Software Engineer'} 
+                        time={'November 2022 - Present'} 
+                        company={'GenesisCare'} 
+                        summary={[
+                            "- Developed scalable and user-friendly full stack software solutions used by hundreds of providers across the world using .NET and JavaScript frameworks. ", <br/>, 
+                            "- Conducted code reviews and knowledge transfer sessions to teach best practices while improving code quality and maintainability.", <br/>,
+                            "- Wrote and maintained technical documentation for internal software products including installation guides, training materials, and product documentation. ", <br/>,
+                            "- Collaborated with a team of eight to update existing applications and modernize existing applications for treatment centers across the US decreasing time spent in documentation by 50%.", <br/>,
+                            
+                            ]} 
+                    />
+                </Row>
+                
                 <Row className='timelineRow'>
                     <Entry 
                         title={'Software Engineer'} 
                         time={'January 2022 - September 2022'} 
                         company={'Rev Tech'} 
                         summary={[
-                            "- Developed applications for a wide range of clients balancing four simultaneous projects working in multiple frameworks and languages in Agile development cycle. ", <br/>, 
+                            "- Demonstrated ability to successfully develop software solutions for diverse clientele, simultaneously balancing up to four concurrent projects in multiple frameworks and languages within Agile development cycles.  ", <br/>, 
                             "- Solved internal onboarding and inventory management system issues in first month by designing and implementing an application on Power Platform reducing onboarding time by 50% and used for 100% of new hires since release.", <br/>,
                             "- Adapted Seattle Office of Sustainability & Environment energy metric tracking fullstack web application to implement more than ten new features and fix downed systems using C# and ASP.NET/MVC while tracking tickets with Jira. ", <br/>,
                             "- Coordinated needs of two stakeholder groups to design and build the front-end of inventory management system for Providence Health & Services utilizing React and integrating REST API using .NET and C#.", <br/>,
-                            "- Developed application for Seattle Office of Arts and Culture with two other engineers using Jira to collect data from over a dozen systems to act as an interface to organize, filter, and access said data.", <br/>,
                             ]} 
                     />
                 </Row>
-                <Row className='timelineRow'>
-                    
-                    <Entry 
-                        title={'Specialist'} 
-                        time={'October 2021 - December 2021'} 
-                        company={'Apple'} 
-                        summary={[
-                            "- Furthered strong interest in technology to educate, advise, and help customers set up their devices from a range of over twenty products. ", <br/>, 
-                            "- Employed strong communicative and problem-solving abilities to rapidly assess and provide solutions for hundreds of customers totaling in over $20,000 of individual sales per day. "
-                        ]} 
-                    />
-                </Row>
-                <Row className='timelineRow'>
-                    <Entry 
-                        title={'Sales Associate'} 
-                        time={'Nov 2020 - Aug 2021'} 
-                        company={'Uniqlo'} 
-                        summary={[
-                            "Troubleshooted and supported guests with our app on their device, led in customer service expectations, and contributed to sales' goals of $60,000 per day while adapting to over forty guests' needs per hour."
-                            
-                        ]} 
-                    />
-                </Row>
+                
                 <Row className='timelineRow'>
                     <Entry 
                         title={'Research Assistant'} 
@@ -94,3 +87,31 @@ const Timeline = () => {
 }
 
 export default Timeline;
+
+{
+    /*
+<Row className='timelineRow'>
+                    
+                    <Entry 
+                        title={'Specialist'} 
+                        time={'October 2021 - December 2021'} 
+                        company={'Apple'} 
+                        summary={[
+                            "- Furthered strong interest in technology to educate, advise, and help customers set up their devices from a range of over twenty products. ", <br/>, 
+                            "- Employed strong communicative and problem-solving abilities to rapidly assess and provide solutions for hundreds of customers totaling in over $20,000 of individual sales per day. "
+                        ]} 
+                    />
+                </Row>
+                <Row className='timelineRow'>
+                    <Entry 
+                        title={'Sales Associate'} 
+                        time={'Nov 2020 - Aug 2021'} 
+                        company={'Uniqlo'} 
+                        summary={[
+                            "Troubleshooted and supported guests with our app on their device, led in customer service expectations, and contributed to sales' goals of $60,000 per day while adapting to over forty guests' needs per hour."
+                            
+                        ]} 
+                    />
+                </Row>
+    */
+}
